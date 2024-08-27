f90111faeb28140fd3dd2afc37bf3399d9336c6b
SIMOOX01 <essalmisimo0@gmail.com>
2024-08-27 02:39:11 -0700

    NoSQL4
    4

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""this module contains the function log_stats"""
+from pymongo import MongoClient
+
+
+def print_stats(mongo_collection):
+    """print some stats about Nginx logs stored in MongoDB"""
+    total_logs = mongo_collection.count_documents({})
+    print(f'{total_logs} logs')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = mongo_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_check_count = mongo_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
+    
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    collection = db.nginx
+    print_stats(collection)
+
